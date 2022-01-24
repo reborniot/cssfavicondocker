e01bdf6e012c46c7648addc4f0d4b5f39d4e7d12
İbrahim Nergiz <contact@ibrahimnergiz.com>
2022-01-24 18:12:03 +0300

    t

--- app/controllers/favicon.controller.js
+++ app/controllers/favicon.controller.js
@@ -13,60 +13,60 @@ exports.getFavIcon = (req, res) => {
     return;
   }
 
-  function icoParser() {
-    request(url, function (err, resp, body) {
-      if (resp.statusCode === 200) {
-        let dom = new JSDOM(body);
-        let favurl = dom.window.document.querySelector('[rel*=icon]')?.href;
+  // function icoParser() {
+  //   request(url, function (err, resp, body) {
+  //     if (resp.statusCode === 200) {
+  //       let dom = new JSDOM(body);
+  //       let favurl = dom.window.document.querySelector('[rel*=icon]')?.href;
 
-        if(!favurl) {
-          fullpath = url + "/favicon.ico";
-        } else {
-          fullpath = favurl.split("/")[0] == "" ? url + favurl : favurl;
-        }
-        // Promise.resolve(fullpath);
-        return fullpath;
-      }
-    });
-  }
+  //       if(!favurl) {
+  //         fullpath = url + "/favicon.ico";
+  //       } else {
+  //         fullpath = favurl.split("/")[0] == "" ? url + favurl : favurl;
+  //       }
+  //       // Promise.resolve(fullpath);
+  //       return fullpath;
+  //     }
+  //   });
+  // }
 
-  function icoChecker() {
-    request(url + '/favicon.ico', function (err, resp, body) {
-      if (resp.statusCode === 200) {
-        let fullpath = url + "/favicon.ico";
-        // Promise.resolve(fullpath);
-        return fullpath;
-      } else {
-        icoParser();
-      }
-    });
-  }
+  // function icoChecker() {
+  //   request(url + '/favicon.ico', function (err, resp, body) {
+  //     if (resp.statusCode === 200) {
+  //       let fullpath = url + "/favicon.ico";
+  //       // Promise.resolve(fullpath);
+  //       return fullpath;
+  //     } else {
+  //       icoParser();
+  //     }
+  //   });
+  // }
 
-  let iconFullPath = icoChecker();
-  res.send("<img src='" + iconFullPath + "'>");
+  // let iconFullPath = icoChecker();
+  // res.send("<img src='" + iconFullPath + "'>");
 
   // Promise.resolve(icoChecker()).then((fullpath) => {res.send("<img src='" + fullpath + "'>")});
 
-  // request(url + '/favicon.ico', function (err, resp, body) {
-  //   if (resp.statusCode === 200) {
-  //     let fullpath = url + "/favicon.ico";
-  //     res.send("<img src='" + fullpath + "'>");
-  //   } else {
-  //     request(url, function (err, resp, body) {
-  //       if (resp.statusCode === 200) {
-  //         let dom = new JSDOM(body);
-  //         let favurl = dom.window.document.querySelector('[rel*=icon]')?.href;
-  //         let fullpath;
+  request(url + '/favicon.ico', function (err, resp, body) {
+    if (resp.statusCode === 200) {
+      let fullpath = url + "/favicon.ico";
+      res.send("<img src='" + fullpath + "'>");
+    } else {
+      request(url, function (err, resp, body) {
+        if (resp.statusCode === 200) {
+          let dom = new JSDOM(body);
+          let favurl = dom.window.document.querySelector('[rel*=icon]')?.href;
+          let fullpath;
     
-  //         if(!favurl) {
-  //           fullpath = url + "/favicon.ico";
-  //         } else {
-  //           fullpath = favurl.split("/")[0] == "" ? url + favurl : favurl;
-  //         }
+          if(!favurl) {
+            fullpath = url + "/favicon.ico";
+          } else {
+            fullpath = favurl.split("/")[0] == "" ? url + favurl : favurl;
+          }
     
-  //         res.send("<img src='" + fullpath + "'>");
-  //       }
-  //     });
-  //   }
-  // });
+          res.send("<img src='" + fullpath + "'>");
+        }
+      });
+    }
+  });
 };
